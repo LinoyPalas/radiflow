5d677c213160809e5b78702f96cf4d9a23e03754
Linoy Palas <50657183+LinoyPalas@users.noreply.github.com>
2023-02-04 18:24:05 +0200

    add print to writer

--- waiter.py
+++ waiter.py
@@ -11,4 +11,7 @@ class Waiter:
         print("Waiter", self.id, "starts serving Pizza", pizza.order_id, "at",
               time.strftime("%H:%M:%S", time.gmtime(order_time)))
         time.sleep(5)
+        print("Waiter finishes serving Pizza", pizza.order_id, "at",
+              time.strftime("%H:%M:%S", time.gmtime(order_time + 5)))
+        self.busy = False
         return order_time + 5
