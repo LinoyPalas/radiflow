c3c6587845d989a0f6a2bdefa4dfc9283739c5de
Linoy Palas <50657183+LinoyPalas@users.noreply.github.com>
2023-02-04 17:47:09 +0200

    create waiter class
    create waiter class, add changes to oven class

--- Oven.py
+++ Oven.py
@@ -1,5 +1,6 @@
 import time
 
+
 class Oven:
     def __init__(self):
         self.busy = False
@@ -8,6 +9,7 @@ class Oven:
         self.busy = True
         print("Oven starts baking Pizza", pizza.order_id, "at", time.strftime("%H:%M:%S", time.gmtime(order_time)))
         time.sleep(10)
-        print("Oven finishes baking Pizza", pizza.order_id, "at", time.strftime("%H:%M:%S", time.gmtime(order_time + 10)))
+        print("Oven finishes baking Pizza", pizza.order_id, "at",
+              time.strftime("%H:%M:%S", time.gmtime(order_time + 10)))
         self.busy = False
         return order_time + 10
---
+++ Waiter.py
@@ -0,0 +1,13 @@
+import time
+
+
+class Waiter:
+    def __init__(self, id):
+        self.id = id
+        self.busy = False
+
+    def serve_pizza(self, pizza, order_time):
+        self.busy = True
+        print("Waiter", self.id, "starts serving Pizza", pizza.order_id, "at",
+              time.strftime("%H:%M:%S", time.gmtime(order_time)))
+        time.sleep(5)
